1d0ed5ca84bf097772508e0ca1752056ae47dd96
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-23 15:05:18 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -43,6 +43,8 @@ import os
 import asyncio
 from telegraph import upload_file
 import pyrogram
+import asyncio
+from pyrogram import Client, filters, enums 
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
@@ -1600,12 +1602,48 @@ async def pin(_, message: Message):
     notify = not any(arg in args for arg in ('loud', 'notify'))
     await message.reply_to_message.pin(disable_notification=notify)
 
-
 @app.on_message(filters.command("unpin"))
 async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.unpin()
+
+
+@app.on_message(filters.command("purge") & (filters.group | filters.channel))                   
+async def purge(client, message):
+    if message.chat.type not in ((enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL)):
+        return
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    status_message = await message.reply_text("...", quote=True)
+    await message.delete()
+    message_ids = []
+    count_del_etion_s = 0
+
+    if message.reply_to_message:
+        for a_s_message_id in range(message.reply_to_message.id, message.id):
+            message_ids.append(a_s_message_id)
+            if len(message_ids) == "100":
+                await client.delete_messages(
+                    chat_id=message.chat.id,
+                    message_ids=message_ids,
+                    revoke=True
+                )
+                count_del_etion_s += len(message_ids)
+                message_ids = []
+        if len(message_ids) > 0:
+            await client.delete_messages(
+                chat_id=message.chat.id,
+                message_ids=message_ids,
+                revoke=True
+            )
+            count_del_etion_s += len(message_ids)
+    await status_message.edit_text(f"deleted {count_del_etion_s} messages")
+    await asyncio.sleep(5)
+    await status_message.delete()
+
 
 #@client.on(events.NewMessage(pattern='/reklam'))
 #async def handler(event):	
