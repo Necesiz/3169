14fe0e4ba5c7eee9a391037b86e41c76485344ed
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-24 03:07:06 +0400

    Create MultiAz.py

---
+++ languages/MultiAz.py
@@ -0,0 +1,11 @@
+SHIP_MSG = "**Budur əsl aşiqlər. {} ilə {} arasında %{} faizində bir sevgi var 😍🥰.**"
+
+
+
+SHIP_ADMIN = "**Əsl Aşiqlər:** 👑 {} 👑 ❤️‍🩹 {}\n\n__Deyəsən, kimsə gözünü qrupun sahibinə dikib.  Bu sevgi tam yüzdə yüzlük bir sevgi__"
+SHIP_TEXT_1 = "**Aşiqlər:** {} 💔 {}\n\n__Aranızın %{} yaxşı olmadığını hiss edirəm. Zəhmət olmasa bir az daha yaxşı yola getməyə çalışın 🥺__"
+
+SHIP_TEXT_2 =  "**Aşiqlər:** {} {} {}\n\n__Bu %{} nisbət mənə nəyəsə nail ola biləcəyinizi hiss etdirdi. Vaz keçmədən bu yola davam edin. ☺️__"
+SHIP_TEXT_3 =  "**Aşiqlər:** {} {} {}\n\n__Bu %{} nisbət mənə sizin bir-birinizə qarşı hisslərin olduğunu hiss etdirdi. Düşünürəm ki, bir-birinizə ürəyinizi tökməlisiniz. 😉__"
+SHIP_TEXT_4 =  "**Aşiqlər:** {} {} {}\n\n__Bu %{} nisbət sizin çox yaxşı cüt olduğunuzu söyləyir! Bir-birinizə olan sevginiz həmişə belə yüksək olsun ☺️__"
+SHIP_TEXT_5 =  '**Aşiqlər:** {} {} {}\n\n__Bu %{} nisbət mənə elə hiss etdirdi ki, siz gözəl bir evliliyə gedəcəksiz. Müslüm Gürsəsin dediyi kimi; “Belə sevgi dünyada heç vaxt görülməyib!" 😍__'
