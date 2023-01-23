ca8d6eaf7031bafe73d312dc0397f8560ee92029
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-23 13:58:13 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -1592,41 +1592,11 @@ async def uploadvid(client, message):
 
 
 
-@client.on(events.NewMessage(pattern='/(?i)quiz')) 
-async def quiz(event):
-    # get the sender
-    sender = await event.get_sender()
-    SENDER = sender.id
-
-    # Start a conversation
-    async with client.conversation(await event.get_chat(), exclusive=True) as conv:
-        # get two random numbers between 1 and 10
-        rand1 = randint(1,10)
-        rand2 = randint(1,10)
-        # make the sum
-        sum = rand1+rand2
-        # make another sum based on two different random numbers. This will be used for the wrong option
-        sum_not_true = randint(1,10) + randint(1,10)
-
-        # To make the position of the button random, let's define two keyboard that activates with 50% probability
-        if(bool(random.getrandbits(1))):
-            keyboard = [[Button.inline("{}".format(sum), sum)], [Button.inline("{}".format(sum_not_true), sum_not_true)]]
-        else:
-            keyboard = [[Button.inline("{}".format(sum_not_true), sum_not_true)],[Button.inline("{}".format(sum), sum)]]
-
-        text = "<b>Quiz time</b> 🤖\n{} + {} = ?\n".format(str(rand1), str(rand2))
-        await conv.send_message(text, buttons=keyboard, parse_mode='html')
-        press = await conv.wait_event(press_event(SENDER))
-        choice = str(press.data.decode("utf-8"))
-
-        if(choice == str(sum)):
-            await conv.send_message("Correct Answer!", parse_mode='html')
-        else:
-            await conv.send_message("Nope, i won!", parse_mode='html')
-
-        await conv.cancel_all()
-        return 
-
+@app.on_message(filters.command('kicl'))
+def kick(bot,msg):
+    if msg.reply_to_message:
+        user = msg.reply_to_message.from_user.id
+        bot.kick_chat_member(msg.chat.id,user)
 
 #@client.on(events.NewMessage(pattern='/reklam'))
 #async def handler(event):	
