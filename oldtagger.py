b7cec04c9cda588adf544e209d6fc47e4161729c
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-24 01:07:57 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -1624,26 +1624,8 @@ async def unpin(_, message: Message):
 
 
 
-emojis = {
-    "moon": list("🌗🌘🌑🌒🌓🌔🌕🌖"),
-    "clock": list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"),
-    "thunder": list("☀️🌤️⛅🌥️☁️🌩️🌧️⛈️⚡🌩️🌧️🌦️🌥️⛅🌤️☀️"),
-    "earth": list("🌏🌍🌎🌎🌍🌏🌍🌎"),
-    "heart": list("❤️🧡💛💚💙💜🖤"),
-}
-emoji_commands = [x for x in emojis]
 
-
-@Client.on_message(filters.command(emoji_commands, ".") & filters.me)
-async def emoji_cycle(bot: Client, message: Message):
-    deq = deque(emojis[message.command[0]])
-    try:
-        for _ in range(randint(16, 32)):
-            await asyncio.sleep(0.3)
-            await message.edit("".join(deq), parse_mode=None)
-            deq.rotate(1)
-    except Exception:
-        await message.delete()
+	
 
 
 
