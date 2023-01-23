d771990c124efa7f0f38d587b057a76c2faf1cf3
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-24 01:55:12 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -79,6 +79,7 @@ RUN_STRINGS = (
     "Polad Həşimov",
     "Mubariz İbrahimov",
     "Muxtar Qasımlı",
+    "Xudayar Yusifzade",
 )
 
 ALIVE = (
@@ -1640,6 +1641,25 @@ async def runs(_, message):
     else:
         await message.reply_text(effective_string)
 	
+
+def aesthetify(string):
+    PRINTABLE_ASCII = range(0x21, 0x7F)
+    for c in string:
+        c = ord(c)
+        if c in PRINTABLE_ASCII:
+            c += 0xFF00 - 0x20
+        elif c == ord(" "):
+            c = 0x3000
+        yield chr(c)
+
+
+@app.on_message(filters.command("ae"))
+async def aesthetic(client, message):
+    status_message = await message.reply_text("...")
+    text = "".join(str(e) for e in message.command[1:])
+    text = "".join(aesthetify(text))
+    await status_message.edit(text)
+
 
 
 
