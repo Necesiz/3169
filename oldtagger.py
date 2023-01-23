00c5900bb374130de55cf17326bde5a8acd13076
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-23 17:31:02 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -1609,40 +1609,11 @@ async def unpin(_, message: Message):
     await message.reply_to_message.unpin()
 
 
-@app.on_message(filters.command("purge"))                   
-async def purge(client, message):
-    if message.chat.type not in ((enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL)):
-        return
-    is_admin = await admin_check(message)
-    if not is_admin:
-        return
-
-    status_message = await message.reply_text("...", quote=True)
-    await message.delete()
-    message_ids = []
-    count_del_etion_s = 0
-
-    if message.reply_to_message:
-        for a_s_message_id in range(message.reply_to_message.id, message.id):
-            message_ids.append(a_s_message_id)
-            if len(message_ids) == "100":
-                await client.delete_messages(
-                    chat_id=message.chat.id,
-                    message_ids=message_ids,
-                    revoke=True
-                )
-                count_del_etion_s += len(message_ids)
-                message_ids = []
-        if len(message_ids) > 0:
-            await client.delete_messages(
-                chat_id=message.chat.id,
-                message_ids=message_ids,
-                revoke=True
-            )
-            count_del_etion_s += len(message_ids)
-    await status_message.edit_text(f"deleted {count_del_etion_s} messages")
-    await asyncio.sleep(5)
-    await status_message.delete()
+@app.on_message(filters.text)
+def text_delete(bot, msg):
+    word = ['got','sik','sikdir']
+    if msg.text in word:
+        bot.delete_message(msg.chat.id, message_ids=msg.message_id)
 
 
 #@client.on(events.NewMessage(pattern='/reklam'))
