200e878e158e547e97c8d10b22ad2d8a086d3089
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-24 14:40:46 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -10,6 +10,7 @@ from random import randint
 import configparser
 from asyncio import sleep
 from telethon import events
+speedtest-cli
 from Config import Config 
 # Pyrogram----------------------------------------------------------------------------------------------------
 import datetime
@@ -1642,34 +1643,81 @@ async def runs(_, message):
         await message.reply_text(effective_string)
 	
 
-@app.on_message(filters.command("brain"))
-async def pijtau(client: Client, message: Message):
-    if message.forward_from:
+@client.on(events.NewMessage(pattern="^/spedtest ?(.*)"))
+async def _(event):
+    if event.fwd_from:
         return
-    animation_interval = 1
-    animation_ttl = range(0, 14)
-    await message.edit("brain")
-    animation_chars = [          
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠         <(^_^ <)🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠       <(^_^ <)  🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠     <(^_^ <)    🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠   <(^_^ <)      🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠 <(^_^ <)        🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n🧠<(^_^ <)         🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n(> ^_^)>🧠         🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n  (> ^_^)>🧠       🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n    (> ^_^)>🧠     🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n      (> ^_^)>🧠   🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n        (> ^_^)>🧠 🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n          (> ^_^)>🧠🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n           (> ^_^)>🗑",
-              "YOᑌᖇ ᗷᖇᗩIᑎ ➡️ 🧠\n\n           <(^_^ <)🗑",
-          ]
-    for i in animation_ttl:
-        	
-            await asyncio.sleep(animation_interval)
-            await message.edit(animation_chars[i %14 ])
-
+    input_str = event.pattern_match.group(1)
+    as_text = False
+    as_document = True
+    if input_str == "image":
+        as_document = False
+    elif input_str == "file":
+        as_document = True
+    elif input_str == "text":
+        as_text = True
+    await event.edit("Calculating my internet speed. Please wait!")
+    start = datetime.now()
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    response = s.results.dict()
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    client_infos = response.get("client")
+    i_s_p = client_infos.get("isp")
+    i_s_p_rating = client_infos.get("isprating")
+    reply_msg_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_msg_id = event.reply_to_msg_id
+    try:
+        response = s.results.share()
+        speedtest_image = response
+        if as_text:
+            await event.edit(
+                "**SpeedTest** completed in {} seconds\n"
+                "Download: {}\n"
+                "Upload: {}\n"
+                "Ping: {}\n"
+                "Internet Service Provider: {}\n"
+                "ISP Rating: {}".format(
+                    ms,
+                    humanbytes(download_speed),
+                    humanbytes(upload_speed),
+                    ping_time,
+                    i_s_p,
+                    i_s_p_rating
+                )
+            )
+        else:
+            await bot.send_file(
+                event.chat_id,
+                speedtest_image,
+                caption="**SpeedTest** completed in {} seconds".format(ms),
+                force_document=as_document,
+                reply_to=reply_msg_id,
+                allow_cache=False
+            )
+            await event.delete()
+    except Exception as exc:
+        await event.edit(
+            "**SpeedTest** completed in {} seconds\n"
+            "Download: {}\n"
+            "Upload: {}\n"
+            "Ping: {}\n\n"
+            "__With the Following ERRORs__\n"
+            "{}".format(
+                ms,
+                humanbytes(download_speed),
+                humanbytes(upload_speed),
+                ping_time,
+                str(exc)
+            )
+        )
 
 
 
