150abd3fcf6569621547921821f1fa6be753a017
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-25 02:20:50 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -61,6 +61,7 @@ import aiohttp
 from pyrogram import filters
 from cryptography.fernet import Fernet
 from AykhanPro.komekci import random_line
+from sorular import D_LİST, C_LİST
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
@@ -1678,9 +1679,99 @@ async def meslehet(_, message):
 async def anekdod(_, message):
     await message.reply_text((await random_line('AykhanPro/txt/anekdod.txt')))
 
-@client.on(events.NewMessage(pattern='(?i)salam.+'))
-async def yeni_mesaj(event: events.NewMessage.Event):
-    await event.delete()
+# Dc Komutu İcin Olan Buttonlar
+def d_or_c(user_id):
+	BUTTON = [[InlineKeyboardButton(text="? Doğruluk", callback_data = " ".join(["d_data",str(user_id)]))]]
+	BUTTON += [[InlineKeyboardButton(text="?? Cesaret", callback_data = " ".join(["c_data",str(user_id)]))]]
+	return InlineKeyboardMarkup(BUTTON)
+
+# Dc Komutunu Oluşturalım
+@app.on_message(filters.command("dc"))
+async def _(client, message):
+	user = message.from_user
+
+	await message.reply_text(text="{} İstediğin Soru Tipini Seç!".format(user.mention),
+		reply_markup=d_or_c(user.id)
+		)
+
+# Buttonlarımızı Yetkilendirelim
+@app.on_callback_query()
+async def _(client, callback_query):
+	d_soru=random.choice(D_LİST) # Random Bir Doğruluk Sorusu Seçelim
+	c_soru=random.choice(C_LİST) # Random Bir Cesaret Sorusu Seçelim
+	user = callback_query.from_user # Kullanıcın Kimliğini Alalım
+
+	c_q_d, user_id = callback_query.data.split() # Buttonlarımızın Komutlarını Alalım
+
+	# Sorunun Sorulmasını İsteyen Kişinin Komutu Kullanan Kullanıcı Olup Olmadığını Kontrol Edelim
+	if str(user.id) == str(user_id):
+		# Kullanıcının Doğruluk Sorusu İstemiş İse Bu Kısım Calışır
+		if c_q_d == "d_data":
+			await callback_query.answer(text="Doğruluk Sorusu İstediniz", show_alert=False) # İlk Ekranda Uyarı Olarak Gösterelim
+			await client.delete_messages(
+				chat_id=callback_query.message.chat.id,
+				message_ids=callback_query.message.message_id) # Eski Mesajı Silelim
+
+			await callback_query.message.reply_text("**{user} Doğruluk Sorusu İstedi:** __{d_soru}__".format(user=user.mention, d_soru=d_soru)) # Sonra Kullanıcıyı Etiketleyerek Sorusunu Gönderelim
+			return
+
+		if c_q_d == "c_data":
+			await callback_query.answer(text="Cesaret Sorusu İstediniz", show_alert=False)
+			await client.delete_messages(
+				chat_id=callback_query.message.chat.id,
+				message_ids=callback_query.message.message_id)
+			await callback_query.message.reply_text("**{user} Cesaret Sorusu İstedi:** __{c_soru}__".format(user=user.mention, c_soru=c_soru))
+			return
+
+
+	# Buttonumuza Tıklayan Kisi Komut Calıştıran Kişi Değil İse Uyarı Gösterelim
+	else:
+		await callback_query.answer(text="Komutu Kullanan Kişi Sen Değilsin!!", show_alert=False)
+		return
+
+############################
+    # Sudo islemleri #
+@app.on_message(filters.command("cekle"))
+async def _(client, message):
+  global MOD
+  user = message.from_user
+  
+  if user.id not in OWNER_ID:
+    await message.reply_text("**[?]** **Sen Yetkili Birisi degilsin!!**")
+    return
+  MOD="cekle"
+  await message.reply_text("**[?]** **Eklenmesini istedigin Cesaret Sorunu Giriniz!**")
+  
+@app.on_message(filters.command("dekle"))
+async def _(client, message):
+  global MOD
+  user = message.from_user
+  
+  if user.id not in OWNER_ID:
+    await message.reply_text("**[?]** **Sen Yetkili Birisi degilsin!!**")
+    return
+  MOD="cekle"
+  await message.reply_text("**[?]** **Eklenmesini istedigin Dogruluk Sorunu Giriniz!**")
+
+@app.on_message(filters.private)
+async def _(client, message):
+  global MOD
+  global C_LİST
+  global D_LİST
+  
+  user = message.from_user
+  
+  if user.id in OWNER_ID:
+    if MOD=="cekle":
+      C_LİST.append(str(message.text))
+      MOD=None
+      await message.reply_text("**[?]** __Metin Cesaret Sorusu Olarak Eklendi!__")
+      return
+    if MOD=="dekle":
+      C_LİST.append(str(message.text))
+      MOD=None
+      await message.reply_text("**[?]** __Metin Dogruluk Sorusu Olarak Eklendi!__")
+      return
 
 #@(events.NewMessage(pattern='/reklam'))
 #async def handler(event):	
