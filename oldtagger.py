c89ddaa996272cb8c29550b0821fbc2452bac27e
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-09 17:32:58 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -83,7 +83,23 @@ from pyrogram.types import ChatPermissions
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from helper.fotnt_string import Fonts
 import asyncio
-import random
+from pyrogram import filters
+from aiohttp import ClientSession
+from pyrogram import Client as app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from asyncio import gather
+from datetime import datetime, timedelta
+from io import BytesIO
+from math import atan2, cos, radians, sin, sqrt
+from os import execvp
+from random import randint
+from re import findall
+from re import sub as re_sub
+from sys import executable
+import aiofiles
+import speedtest
+from PIL import Image
+from pyrogram.types import Message
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
@@ -1446,6 +1462,37 @@ async def runs(_, message):
 
 
 
+aiohttpsession = ClientSession()
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+@app.on_message(filters.command("ccarbon"))
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+        )
+    user_id = message.from_user.id
+    m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
+    await message.reply_photo(
+        photo=carbon,
+        caption="**Made by @mkn_bots_updates**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+    )
+    await m.delete()
+    carbon.close()
 
 
 
